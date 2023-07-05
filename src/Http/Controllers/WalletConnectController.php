93d4086275431bc84bb89e973d696f185f87a1c2
Cardano Blockhouse [CBH] <90690011+cardano-blockhouse@users.noreply.github.com>
2023-07-05 15:09:31 +0200

    Only update db rows if the did has changed

--- src/Http/Controllers/WalletConnectController.php
+++ src/Http/Controllers/WalletConnectController.php
@@ -7,6 +7,7 @@ use IAMXID\IamxWalletConnect\Models\IamxIdentityAttribute;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class WalletConnectController extends Controller
@@ -19,48 +20,73 @@ class WalletConnectController extends Controller
             ],[
                 'name' => $request->data['person']['firstname'].' '.$request->data['person']['lastname'],
                 'email' => $request->data['vUID']['id'].'@iamx.id',
-                'password' => Hash::make($request->data['did'].$request->data['person']['firstname'].$request->data['person']['lastname'])
+                'password' => Hash::make($request->data['vUID']['id'].$request->data['person']['firstname'].$request->data['person']['lastname'])
             ]
         );
 
-        // Insert wallet data into table iamx_identity_attributes
-        foreach ($request->data as $parentKey => $parentValue) {
-            if (is_array($parentValue)) {
-                $index = 0;
-                foreach ($parentValue as $childKey => $childValue) {
-                    if (is_array($childValue)) {
-                        foreach ($childValue as $childChildKey => $childchildValue) {
-                            if($childchildValue) {
-                                IamxIdentityAttribute::updateOrCreate(
-                                    [
-                                        'user_id' => $user->id,
-                                        'category' => $parentKey,
-                                        'attribute_name' => $childChildKey,
-                                        'element_number' => $index
-                                    ],
-                                    [
-                                        'attribute_value' => Crypt::encryptString($childchildValue)
-                                    ]
-                                );
+        // Fetch old value of did if exists
+        $oldDID = DB::table('iamx_identity_attributes')
+            ->select('attribute_value')
+            ->where('category', '=', 'root')
+            ->where('attribute_name', '=', 'did')
+            ->where('user_id', '=', $user->id)
+            ->first();
+
+        // Only change stored wallet data if the did has changed
+        if ($oldDID->attribute_value) {
+            if (Crypt::decryptString($oldDID->attribute_value) != $request->data['did']) {
+
+                // Insert or update wallet data into table iamx_identity_attributes
+                foreach ($request->data as $parentKey => $parentValue) {
+                    if (is_array($parentValue)) {
+                        $index = 0;
+                        foreach ($parentValue as $childKey => $childValue) {
+                            if (is_array($childValue)) {
+                                foreach ($childValue as $childChildKey => $childchildValue) {
+                                    if($childchildValue) {
+                                        IamxIdentityAttribute::updateOrCreate(
+                                            [
+                                                'user_id' => $user->id,
+                                                'category' => $parentKey,
+                                                'attribute_name' => $childChildKey,
+                                                'element_number' => $index
+                                            ],
+                                            [
+                                                'attribute_value' => Crypt::encryptString($childchildValue)
+                                            ]
+                                        );
+                                    }
+                                }
+                                $index++;
+
+                            } else {
+                                if($childValue) {
+                                    IamxIdentityAttribute::updateOrCreate(
+                                        [
+                                            'user_id' => $user->id,
+                                            'category' => $parentKey,
+                                            'attribute_name' => $childKey
+                                        ],
+                                        [
+                                            'attribute_value' => Crypt::encryptString($childValue)
+                                        ]
+                                    );
+                                }
                             }
-                        }
-                        $index++;
 
+                        }
                     } else {
-                        if($childValue) {
-                            IamxIdentityAttribute::updateOrCreate(
-                                [
-                                    'user_id' => $user->id,
-                                    'category' => $parentKey,
-                                    'attribute_name' => $childKey
-                                ],
-                                [
-                                    'attribute_value' => Crypt::encryptString($childValue)
-                                ]
-                            );
-                        }
+                        IamxIdentityAttribute::updateOrCreate(
+                            [
+                                'user_id' => $user->id,
+                                'category' => 'root',
+                                'attribute_name' => $parentKey
+                            ],
+                            [
+                                'attribute_value' => Crypt::encryptString($parentValue)
+                            ]
+                        );
                     }
-
                 }
             }
         }
